8e1df7703df71cbc384164be5d28e4d1ddded0d6
SourceDev <src.sovannarith.it@gmail.com>
2024-06-14 15:24:30 +0700

    feature(main layout): add video play ground to home page)

--- app/[locale]/(home)/page.tsx
+++ app/[locale]/(home)/page.tsx
@@ -1,14 +1,12 @@
 import { useTranslations } from "next-intl";
-import Image from "next/image";
-import { useTranslation } from "react-i18next";
 
 export default function Home() {
   const t = useTranslations("HomePage");
-
   return (
     <>
-    <h1>Default home page ខ្ញុំចង់មានស្នេហា​ខ្លាំងណាស់</h1>
-    <h2>{t('title')}</h2>
+    <video autoPlay loop muted>
+      <source src={'_next-video/cut.mp4'} type="video/mp4"/>
+    </video>
     </>
     
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
+import { withNextVideo } from "next-video/process";
+// next.config.js
+
 /** @type {import('next').NextConfig} */
 
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
- 
-export default withNextIntl(nextConfig);
+
+export default withNextVideo(withNextIntl(nextConfig));
+
+
+
+
 
